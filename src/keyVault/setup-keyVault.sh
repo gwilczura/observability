0f7df8e6199f87b4f352e40763bb7c0696290404
Grzegorz Wilczura <grzegorz.wilczura@gmail.com>
2024-12-15 16:38:09 +0100

    keyvault values setting shell file

---
+++ src/keyVault/setup-keyVault.sh
@@ -0,0 +1,19 @@
+VAULTNAME=$1
+               
+az keyvault secret set --vault-name $VAULTNAME -n Logging--Elasticsearch--ShipTo--ApiKey    --value 
+az keyvault secret set --vault-name $VAULTNAME -n Logging--Elasticsearch--ShipTo--CloudId   --value 
+
+az keyvault secret set --vault-name $VAULTNAME -n Bff--ApiKey                               --value 
+az keyvault secret set --vault-name $VAULTNAME -n Bff--ServicePrincipal--ClientSecret       --value 
+
+az keyvault secret set --vault-name $VAULTNAME -n Prices--ServicePrincipal--ClientSecret    --value 
+az keyvault secret set --vault-name $VAULTNAME -n ConnectionStrings--Prices                 --value 
+az keyvault secret set --vault-name $VAULTNAME -n ConnectionStrings--PricesBus              --value 
+
+az keyvault secret set --vault-name $VAULTNAME -n Products--ServicePrincipal--ClientSecret  --value 
+az keyvault secret set --vault-name $VAULTNAME -n ConnectionStrings--Products               --value 
+az keyvault secret set --vault-name $VAULTNAME -n ConnectionStrings--ProductsBus            --value 
+
+az keyvault secret set --vault-name $VAULTNAME -n Stock--ServicePrincipal--ClientSecret     --value 
+az keyvault secret set --vault-name $VAULTNAME -n ConnectionStrings--Stock                  --value 
+az keyvault secret set --vault-name $VAULTNAME -n ConnectionStrings--StockBus               --value 
